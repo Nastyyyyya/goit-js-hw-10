51048da72e79f9bea48b1c7ea163d4011cbe7f1a
Anastasia Paslavska <a.paslavska2206@gmail.com>
2025-01-07 02:19:49 +0200

    add homework files

---
+++ .github/src/js/2-snackbar.js
@@ -0,0 +1,44 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const delay = parseInt(form.delay.value, 10);
+  const state = form.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        progressBar: false,
+        icon: null,
+        backgroundColor: '#59A10D',
+        messageColor: 'white',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        progressBar: false,
+        icon: null,
+        backgroundColor: '#EF4040',
+        messageColor: 'white',
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
